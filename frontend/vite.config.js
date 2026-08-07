9066df866dc9fc90cf8ead44f144f1cd69c96943
Emmanuel <okoosiemmanuel@gmail.com>
2026-08-07 22:22:01 +0100

    feat: add @rolldown/plugin-babel to package.json and package-lock.json, update Dockerfile to remove legacy-peer-deps, and configure Vite to use Babel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -7,5 +8,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] })
   ],
 })
