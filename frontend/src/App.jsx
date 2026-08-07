3a993a67458abb5ecb60bd6ce59138d86de2431b
Emmanuel <okoosiemmanuel@gmail.com>
2026-08-07 20:34:20 +0100

    feat: add theme and wallpaper context providers, implement routing for authentication and chat pages
    - Added ThemeProvider and WallpaperProvider for managing theme and wallpaper state.
    - Integrated react-hot-toast for notifications.
    - Implemented routing with react-router for ChatPage and AuthPage.
    - Updated package.json with new dependencies including Tailwind CSS and HeroUI.
    - Added various wallpapers and sound assets to the public directory.
    - Created initial AuthPage and ChatPage components.
    - Added heroui theme presets for styling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { useAuth, } from '@clerk/react'
+import { ThemeProvider } from './context/ThemeContext'
+import { WallpaperProvider } from './context/WallpaperContext'
+import { Routes, Route, Navigate } from "react-router";
+import { Toaster } from "react-hot-toast";
+import { useEffect } from 'react';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
 
 const App = () => {
+  const { isSignedIn, isLoaded } = useAuth();
+
+
+  // useEffect(() => {
+  //   if (!isLoaded) return;
+
+  //   if (!isSignedIn) checkAuth();
+  //   else clearAuth();
+  // }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
   return (
-    <>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode='modal' />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
